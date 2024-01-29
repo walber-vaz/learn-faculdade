16157bdc9e647c362a47e00ede6644633e1c8b89
Walber Vaz da Silva <wvs.walber@gmail.com>
2024-01-29 20:45:34 -0300

    feat: add JavaScript operators examples

---
+++ 1_trimestre/JavaScript/operadores.js
@@ -0,0 +1,33 @@
+// Tipo de operadores
+// Operador binário e unário
+// // Binário
+// Operadores aritméticos
+// // Adição +
+let soma = 1 + 2;
+console.log(soma); // 3
+// // Subtração -
+let subtracao = 10 - 5;
+console.log(subtracao); // 5
+// // Multiplicação *
+let multiplicacao = 10 * 5;
+console.log(multiplicacao); // 50
+// // Divisão /
+let divisao = 10 / 2;
+console.log(divisao); // 5
+// // Módulo %
+let resto = 10 % 2;
+console.log(resto); // 0
+
+// // Unário
+let numero = 1;
+console.log(++numero); // 2
+let numero2 = 1;
+console.log(numero2++); // 1
+console.log(numero2); // 2
+
+// Operadores Ternario
+// // (condicao) ? true : false
+let maiorDeIdade = false;
+let idade = 17;
+maiorDeIdade = idade >= 18 ? true : false;
+console.log(maiorDeIdade); // false
